1eed1c56ff53fe0f2d8a73eeb0decaae59ca0724
Melissa Aime <aimemelissa1@gmail.com>
2022-07-19 21:57:38 -0300

    section skills

--- src/Sections/Skills/Skills.js
+++ src/Sections/Skills/Skills.js
@@ -1,17 +1,23 @@
 import { Module } from "../../Components/ModuleSkill/moduleskill"
 import { FaHtml5, FaCss3Alt, FaSass, FaBootstrap, FaGithub, FaJs, FaReact, FaPython } from 'react-icons/fa';
+import { Title } from "../../Components/Title/Title";
 
 export const Skills = () => {
     return(
-        <div className="preback">
-            <Module icon={ <FaHtml5/> } title={"HTML"} percent={90}/>
-            <Module icon={ <FaCss3Alt/> } title={"CSS"} percent={85}/>
-            <Module icon={ <FaSass/> } title={"SASS"} percent={85}/>
-            <Module icon={ <FaBootstrap/> } title={"Bootstrap"} percent={80}/>
-            <Module icon={ <FaGithub/> } title={"GitHub"} percent={75}/>
-            <Module icon={ <FaJs/> } title={"JavaScript"} percent={50}/>
-            <Module icon={ <FaReact/> } title={"React JS"} percent={75}/>
-            <Module icon={ <FaPython/> } title={"Python"} percent={55}/>
+        <div className="preback row">
+        <Title title={"Skills"}/>
+            <div className="col">
+                <Module icon={ <FaHtml5/> } title={"HTML"} percent={90}/>
+                <Module icon={ <FaCss3Alt/> } title={"CSS"} percent={85}/>
+                <Module icon={ <FaSass/> } title={"SASS"} percent={85}/>
+                <Module icon={ <FaBootstrap/> } title={"Bootstrap"} percent={80}/>
+            </div>
+            <div className="col">
+                <Module icon={ <FaGithub/> } title={"GitHub"} percent={75}/>
+                <Module icon={ <FaJs/> } title={"JavaScript"} percent={50}/>
+                <Module icon={ <FaReact/> } title={"React JS"} percent={75}/>
+                <Module icon={ <FaPython/> } title={"Python"} percent={55}/>
+            </div>
         </div>
     )
 }
